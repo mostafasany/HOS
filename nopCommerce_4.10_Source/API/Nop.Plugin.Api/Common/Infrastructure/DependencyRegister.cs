7ecaf3c965d879d05eb38ba9322cde5885b10b6b
mkhodeir <mostafa.khodeir@zalando.de>
2019-01-20 00:46:39 +0100

    MOving Products modules

--- nopCommerce_4.10_Source/API/Nop.Plugin.Api/Common/Infrastructure/DependencyRegister.cs
+++ nopCommerce_4.10_Source/API/Nop.Plugin.Api/Common/Infrastructure/DependencyRegister.cs
@@ -50,7 +50,6 @@ namespace Nop.Plugin.Api.Infrastructure
             builder.RegisterType<ShoppingCartItemApiService>().As<IShoppingCartItemApiService>().InstancePerLifetimeScope();
             builder.RegisterType<OrderItemApiService>().As<IOrderItemApiService>().InstancePerLifetimeScope();
             builder.RegisterType<ProductAttributesApiService>().As<IProductAttributesApiService>().InstancePerLifetimeScope();
-            builder.RegisterType<ProductPictureService>().As<IProductPictureService>().InstancePerLifetimeScope();
             builder.RegisterType<ProductAttributeConverter>().As<IProductAttributeConverter>().InstancePerLifetimeScope();
             builder.RegisterType<SpecificationAttributesApiService>().As<ISpecificationAttributeApiService>().InstancePerLifetimeScope();
             builder.RegisterType<NewsLetterSubscriptionApiService>().As<INewsLetterSubscriptionApiService>().InstancePerLifetimeScope();
