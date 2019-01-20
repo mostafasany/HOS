a415e88e361bd1c4ccaf46118a80b3f54b3cc5c2
mkhodeir <mostafa.khodeir@zalando.de>
2019-01-20 01:03:32 +0100

    Refactor

---
+++ nopCommerce_4.10_Source/API/Nop.Plugin.Api/Modules/Cart/Dto/BaseNopEntityModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Nop.Plugin.Api.DTOs.ShoppingCarts
+{
+    /// <summary>
+    ///     Represents base nopCommerce entity model
+    /// </summary>
+    public class BaseNopEntityModel : BaseNopModel
+    {
+        /// <summary>
+        ///     Gets or sets model identifier
+        /// </summary>
+        public virtual int Id { get; set; }
+    }
+}
