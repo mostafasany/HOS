68ac242a2c5b762a4d872846df00b26fd6a0bc0a
Mostafa Khodeir <mostafa.khodeir@zalando.de>
2019-07-22 00:12:17 +0200

    Adding Menu API

---
+++ nopCommerce_4.20_Source/src/API/Nop.Plugin.Api.Menu/Dto/MenuDto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Nop.Plugin.Api.Common.DTOs;
+
+namespace Nop.Plugin.Api.Menu.Dto
+{
+    public class MenuDto : ISerializableObject
+    {
+        /// <summary>
+        ///     Gets or sets the Main Store Products
+        /// </summary>
+        [JsonProperty("MainStoreProducts")]
+        public List<MenuProductsDto> MainStoreProducts { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the Main Store Products
+        /// </summary>
+        [JsonProperty("MainStoreCategories")]
+        public List<MenuCategoriesDto> MainStoreCategories { get; set; }
+
+        [JsonProperty("ArticlesAndVideos")] public List<MenuArticlesDto> ArticlesAndVideos { get; set; }
+
+
+        [JsonProperty("Exercises")] public List<MenuArticlesDto> Exercises { get; set; }
+
+
+        [JsonProperty("Nutrations")] public List<MenuArticlesDto> Nutrations { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the Main Store Products
+        /// </summary>
+        [JsonProperty("MainStoreBrands")]
+        public List<MenuCategoriesDto> MainStoreBrands { get; set; }
+
+        public string GetPrimaryPropertyName()
+        {
+            return "MenuDto";
+        }
+
+        public Type GetPrimaryPropertyType()
+        {
+            return typeof(List<MenuProductsDto>);
+        }
+    }
+}
